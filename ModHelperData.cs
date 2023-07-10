dca090093a3b348953ba2ef926ff889741fd7067
mend-dev <mendbookdev@gmail.com>
2023-07-10 15:37:53 -0500

    updated modhelperdata & readme

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -1,13 +1,12 @@
 namespace BTD6Rogue;
 
-public static class ModHelperData
-{
+public static class ModHelperData {
     public const string WorksOnVersion = "36.1";
     public const string Version = "1.0.0";
     public const string Name = "BTD6Rogue";
 
     public const string Description = "An empty mod";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "mend-dev"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "BTD6Rogue"; // TODO add your repo name here, also in the download url in README.md
 }
