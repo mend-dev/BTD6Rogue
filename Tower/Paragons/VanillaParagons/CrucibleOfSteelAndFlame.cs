3eb10380c7747a0e6547493535ba2f6b0c33a096
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2024-12-13 14:15:50 -0600

    Create CrucibleOfSteelAndFlame.cs

---
+++ Tower/Paragons/VanillaParagons/CrucibleOfSteelAndFlame.cs
@@ -0,0 +1,8 @@
+﻿using Il2CppAssets.Scripts.Models.Towers;
+
+namespace BTD6Rogue;
+
+public class ApexPlasmaMaster : RogueParagon {
+	public override string DisplayName => "Apex Plasma Master";
+	public override string BaseTowerId => TowerType.DartMonkey;
+}
